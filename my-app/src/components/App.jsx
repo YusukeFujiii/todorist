7ca693cc5e95e5acad513c72b918c17fd0f58e9d
Yusuke Fujii <fujii@baseconnect.in>
2020-07-02 22:14:53 +0900

    SetUpApp.jsx

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Form from './Form';
+import List from './List';
 
 export default class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo =[]
+    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(e) {
+    console.log(e.target.title.value);
+    e.preventDefault();
+  }
   render() {
     return (
       <div className="siimple-box siimple--bg-dark">
         <h1 className="simple-box-title siimple--color-white">Reackt Todo App</h1>
-      <Form/>
+        <Form />
+        <div className="siimple-rule"></div>
+        <List/>
       </div>
     );
   }
